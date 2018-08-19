71939e22c32ca67f6957f4807cc1c9366bf7367b
Sarah Huynh <sarah.huynh@uptake.com>
2018-08-18 21:01:58 -0500

    init

---
+++ app/js/app.js
@@ -0,0 +1,12 @@
+//this initializes the app and registers the modules on which the app depends
+angular.module('F1FeederApp', [
+  'F1FeederApp.services',
+  'F1FeederApp.controllers',
+  'ngRoute'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+	when("/drivers", {templateUrl: "partials/drivers.html", controller: "driversController"}).
+	when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
+	otherwise({redirectTo: '/drivers'});
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,36 @@
+angular.module('F1FeederApp.controllers', []).
+
+  /* Drivers controller */
+/*  $scope: variable is supposed to link your controller and views,
+ it holds all the data that will be used w/in your template
+ Notes: $scope should be replaced with "this":
+ vm = this
+ vm: view module */
+  controller('driversController', function($scope, ergastAPIservice) {
+    $scope.nameFilter = null;
+    $scope.driversList = [];
+    $scope.searchFilter = function (driver) {
+        var re = new RegExp($scope.nameFilter, 'i');
+        return !$scope.nameFilter || re.test(driver.Driver.givenName) || re.test(driver.Driver.familyName);
+    };
+
+    ergastAPIservice.getDrivers().success(function (response) {
+        //Digging into the response to get the relevant data
+        $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    });
+  }).
+
+  /* Driver controller */
+  controller('driverController', function($scope, $routeParams, ergastAPIservice) {
+    $scope.id = $routeParams.id;
+    $scope.races = [];
+    $scope.driver = null;
+
+    ergastAPIservice.getDriverDetails($scope.id).success(function (response) {
+        $scope.driver = response.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]; 
+    });
+
+    ergastAPIservice.getDriverRaces($scope.id).success(function (response) {
+        $scope.races = response.MRData.RaceTable.Races; 
+    }); 
+  });
